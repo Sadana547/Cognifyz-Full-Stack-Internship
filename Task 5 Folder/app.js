5d5f23ce531de6e6d4cb12489ac4067e050981b5
Sadana547 <sadanasaravanan324@gmail.com>
2026-04-16 16:36:24 +0530

    Update app.js

--- Task 5 Folder/app.js
+++ Task 5 Folder/app.js
@@ -37,6 +37,7 @@ app.get("/api/blogs", (req, res) => {
     res.json(blogs);
 });
 
-app.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
